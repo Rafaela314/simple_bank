859aa751d3c542121e915a02010033aa62ec625b
Rafaela Cavalcante <rafaelacavalcante@bitbucket.org>
2025-10-01 17:12:39 -0300

    add util functions to testing

---
+++ util/random.go
@@ -0,0 +1,52 @@
+package util
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+// Create a orivate RNG instance with its own seed
+var rng = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+// RandomInt generates a random integer in the range [min, max]
+func RandomInt(min, max int64) int64 {
+	if max <= min {
+		return min
+	}
+	return min + rng.Int63n(max-min+1)
+}
+
+// RandomString generatess a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+
+}
+
+// RandomOwnerName generates a random owner name
+func RandOwner() string {
+	return RandomString(6)
+}
+
+// RandomOwnerMoney generates a random amount of money
+func RandomMoney() int64 {
+	return RandomInt(0, 100)
+}
+
+// RandomCurrency generates a random currenct code
+func RandomCurency() string {
+	currencies := []string{"EUR", "USD", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+
+}
