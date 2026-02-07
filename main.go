33ceec201a36dbf94514a4e77cb1b65f85cef179
Rafaela Cavalcante <rafaelacavalcante@bitbucket.org>
2026-02-07 06:00:17 +0100

    add main file using viper to retrieve db source and server address

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"simple_bank/api"
+	db "simple_bank/db/sqlc"
+	"simple_bank/util"
+
+	_ "github.com/lib/pq"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
 
 func main() {
-	fmt.Println("Hello")
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := pgxpool.New(context.Background(), config.DBSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err)
+	}
+	defer conn.Close()
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+	if err := server.Start(config.ServerAddress); err != nil {
+		log.Fatal("cannot start server:", err)
+	}
 }
